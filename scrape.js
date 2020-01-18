6ff7e9785fba46d6c4637517838c1d6afd39c996
Queenshiba <haruka0111.2a@gmail.com>
2020-01-18 17:01:17 +0900

    removed loop

--- scrape.js
+++ scrape.js
@@ -12,35 +12,43 @@ request('https://natureworksbest.com/naturopathy-works/food-cravings/', (error, 
 
 
 
-
-
-        var data = $('tbody').text()
+        var data = $('tr').text()
         var arr = []
-        for (let i = 0; i < data.length; i++) {
-            if (data[i] == $('td')) {
-                arr.push(data[i].nodeValue);
-                continue;
-            }
-            arr.push(data[i])
+        var arrp = arr.push(data)
 
-            // console.log(arr)
-        }
+        // try to use for loop but does't work
 
+        // for (let i = 0; i < 10; i++) {
+        //     var splitspace = data.split(',')
+        //     // if (data[i] == $('td')) {
+        //     //     arr.push(data[i].nodeValue);
+        //     //     continue;
+        //     // }
+        //     arr.push(data[i])
+
+            // console.log(arrp)
+        // }
+
+                // extract each target
                 $('tbody').each(function () {
                     const cravingChart = [];
                     const cravingThis = $('tr').children('td').first().text();
                     const reason = $('tr').children('td').slice(1).eq(0).text();
                     const solution = $('tr').children('td').last().text();
 
-                    
+                    // push the target to json form
                     cravingChart.push({
                         cravingThis: cravingThis,
                         reason: reason,
                         solution: solution,
                     });
-
-
             console.log(cravingChart)
+            // look like this
+            // [ { cravingThis: 'Chocolate',
+            // reason: 'Magnesium',
+            // solution:
+            //  'Vitamin C supplements or orange, green and red fruits and vegetables' } ]
+
         })
     }
 });
